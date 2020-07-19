f9521e02d458d6a0e10a7d146b34093e4d254ffa
Rene <renldx@gmail.com>
2020-07-19 18:27:26 -0400

    Refactor & created test project

---
+++ MadbullAPI.Tests.Integration/DbFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MadbullAPI.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace MadbullAPI.Tests.Integration
+{
+    public class DbFixture : IDisposable
+    {
+        public DbFixture()
+        {
+            new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json")
+                .Build()
+                .GetSection("MadbullDatabaseSettings")
+                .Bind(DbSettings);
+
+            DbSettings.DatabaseName = $"test_db_{ Guid.NewGuid() }";
+        }
+
+        public IMadbullDatabaseSettings DbSettings { get; } = new MadbullDatabaseSettings();
+
+        public void Dispose()
+        {
+            var client = new MongoClient(DbSettings.ConnectionString);
+            client.DropDatabase(DbSettings.DatabaseName);
+        }
+    }
+}
---
+++ MadbullAPI.Tests.Integration/ServiceTests.cs
@@ -0,0 +1,70 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using MadbullAPI.Models;
+using MadbullAPI.Services;
+
+namespace MadbullAPI.Tests.Integration
+{
+    public class ServiceTests : IClassFixture<DbFixture>
+    {
+        private readonly DbFixture Fixture;
+
+        public ServiceTests(DbFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        [Fact]
+        public async Task ExerciseServicesCRUD()
+        {
+            var exerciseService = new ExerciseService(Fixture.DbSettings);
+
+            // Create
+
+            var name = "Squat";
+            var type = Enums.ResistanceType.Dynamic;
+            var unit = 20;
+
+            var exercise = exerciseService.Create(new Exercise(name, type, unit));
+
+            Assert.True(exercise is Exercise);
+            Assert.True(exercise.Name == name);
+            Assert.True(exercise.ResistanceType == type);
+            Assert.True(exercise.ResistanceUnit == unit);
+
+            // Read
+
+            var exercises = exerciseService.Get();
+
+            Assert.True(exercises.Any());
+            Assert.True(exercises.All(e => e is Exercise));
+
+            exercise = exerciseService.Get(exercises.First().Id);
+
+            Assert.True(exercise is Exercise);
+            Assert.True(exercise.Id == exercises.First().Id);
+
+            // Update
+
+            var newName = "Deadlift";
+
+            exercise.Name = newName;
+
+            exerciseService.Update(exercise.Id, exercise);
+
+            exercise = exerciseService.Get(exercise.Id);
+
+            Assert.True(exercise is Exercise);
+            Assert.True(exercise.Name == newName);
+
+            // Delete
+
+            exerciseService.Remove(exercise.Id);
+
+            exercise = exerciseService.Get(exercise.Id);
+
+            Assert.True(exercise is null);
+        }
+    }
+}
